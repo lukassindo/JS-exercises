9b98bca2b9b9456a500dc5dce714bd8e16dc358b
Lukasz Trzaska <lukasztrzaska81@gmail.com>
2018-01-04 21:10:42 +0100

    ready do send

--- zad-tablice/js/scripts.js
+++ zad-tablice/js/scripts.js
@@ -2,11 +2,11 @@ var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 
 var allNames = femaleNames.concat(maleNames);
-
 var newName = 'Marian';
 
-
-
-
+if (allNames.indexOf(newName) === -1) {
+	var x = allNames.push(newName);
+}
 
 console.log(allNames);
+console.log('number of items: ' + x)

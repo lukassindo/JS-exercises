b71ec06f4077106cbc2ce35eea6a0655a8135ab2
Lukasz Trzaska <lukasztrzaska81@gmail.com>
2018-01-04 20:02:13 +0100

    first commit

---
+++ zad-tablice/js/scripts.js
@@ -0,0 +1,12 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames);
+
+var newName = 'Marian';
+
+
+
+
+
+console.log(allNames);

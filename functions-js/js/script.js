5fa6570b8eb9366c7fd87a0c86f0ca5ba745cb98
Lukasz Trzaska <lukasztrzaska81@gmail.com>
2018-01-07 13:01:45 +0100

    choinka

---
+++ functions-js/js/script.js
@@ -0,0 +1,14 @@
+
+function drawTree(height) {
+	for (i = 0; i < height; i++) {
+		var star = ' ';
+		for (j = 0; j < i + 1; j++) {
+			var star = star += '*'; 
+		}
+		for (k = 0; k < (height - i); k++) {
+				var star = 'o'+star;
+			}
+		console.log(star);
+	}	
+		
+}

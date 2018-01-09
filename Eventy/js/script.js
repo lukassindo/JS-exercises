ae7f27c408d7e751f9ac6c2708fd8b2c2bac9d93
Lukasz Trzaska <lukasztrzaska81@gmail.com>
2018-01-09 19:58:20 +0100

    poprawki

--- Eventy/js/script.js
+++ Eventy/js/script.js
@@ -1,10 +1,9 @@
 var list = document.getElementById('list');
 var add = document.getElementById('addElem');
-
+var liElement = document.getElementsByTagName('li');
 
 add.addEventListener('click', function(e) {
 	var element = document.createElement('li');
-	var liElement = document.getElementsByTagName('li');
-	element.innerHTML = 'item' + liElement.length;
+	element.innerHTML = 'item ' + liElement.length;
     list.appendChild(element);
 })

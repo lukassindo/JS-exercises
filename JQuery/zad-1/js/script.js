d4b532e7caf3d5605eb68006bfe81216f90f6f4e
Lukasz Trzaska <lukasztrzaska81@gmail.com>
2018-01-16 15:37:21 +0100

    poprawki

--- JQuery/zad-1/js/script.js
+++ JQuery/zad-1/js/script.js
@@ -1,20 +1,20 @@
 
-$("span:even").css('color', 'red');
-$("p").css('margin-left','20px');
+$('span:even').css('color','red');
+$('p').css('margin-left','20px');
 
 
 var paragraphs = $('p');
 paragraphs.each(function(index, element) {
 
-    var button = '<button class="btn btn-primary" data-tmp="' + index + '">Click me</button>'
-    $(element).append(button)
+    var button = '<button class="btn btn-primary" data-tmp="' + index + '">Click me</button>';
+    $(element).append(button);
 
 });
 
 
-$("button:even").css('margin-left', '20px');
-$("button:odd").css('margin-left', '53px');
+$('button:even').css('margin-left', '20px');
+$('button:odd').css('margin-left', '53px');
 
-$("button").click(function(){
-	alert($(this).attr("data-tmp"));
+$('button').click(function(){
+	alert($(this).attr('data-tmp'));
 });
